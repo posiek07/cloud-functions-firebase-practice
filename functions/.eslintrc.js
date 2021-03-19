f4fd422c4e99222a4ba6e5814c29b407356c0c09
posiek07 <daniel.smolinski@hotmail.com>
2021-03-19 19:58:47 +0000

    first commit

---
+++ functions/.eslintrc.js
@@ -0,0 +1,16 @@
+/* eslint-disable quotes */
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'module',
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'google'],
+  rules: {
+    quotes: ['error', 'double'],
+  },
+};
